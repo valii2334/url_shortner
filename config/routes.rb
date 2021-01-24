08998240bda4f7fdea9e1ee62e320e6588616389
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2021-01-24 19:27:54 +0200

    FIX: remove HomeController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :short_urls
+  resources :short_urls, only: [:new, :create, :update]
 
   get '/s/:random_hex', to: 'short_urls#search', as: :short_url_generator
 
